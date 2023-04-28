88dc6274c99126ea18b9cc671c8a0463768395e2
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2023-04-28 08:41:25 -0700

    change

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use web3::Web3;
 use web3::types::Address;
 use crate::erc20_abi::ERC20_ABI;
 use web3::ethabi::Contract as EthabiContract;
+use web3::types::{ BlockId, BlockNumber, Transaction };
 
 async fn connect_to_ethereum_node() -> Web3<Http> {
     let transport = Http::new(
@@ -39,7 +40,41 @@ async fn interact_with_erc20(web3: &Web3<Http>, token_address: &str, user_addres
         .query("balanceOf", user_address, None, web3::contract::Options::default(), None).await
         .unwrap();
     println!("User's token balance: {}", balance);
+}
 
+async fn get_transactions(web3: &Web3<Http>, user_address: &str) {
+    let user_address: Address = user_address.parse().unwrap();
+    let mut transactions: Vec<Transaction> = vec![];
+    let latest_block_number = web3.eth().block_number().await.unwrap();
+    let start_block = web3::types::U256::from(latest_block_number.as_u64()) - web3::types::U256::from(17144990); // Adjust this number based on how far back you want to search
+    for block_number in (start_block.as_u64()..=latest_block_number.as_u64()).rev() {
+        let block = web3
+            .eth()
+            .block(BlockId::Number(BlockNumber::Number(block_number.into())))
+            .await
+            .unwrap();
+        if let Some(block) = block {
+            for transaction_hash in block.transactions {
+                let transaction = web3.eth().transaction(web3::types::TransactionId::Hash(transaction_hash)).await.unwrap();
+                if let Some(transaction) = transaction {
+                    if transaction.from == user_address || transaction.to == Some(user_address) {
+                        transactions.push(transaction);
+                    }
+                }
+            }
+        }
+    }
+    println!("Found {} transactions", transactions.len());
+    for (index, transaction) in transactions.iter().enumerate() {
+        println!(
+            "Transaction {}: Hash: {:?}, From: {:?}, To: {:?}, Value: {:?}",
+            index + 1,
+            transaction.hash,
+            transaction.from,
+            transaction.to,
+            transaction.value
+        );
+    }
 }
 
 #[tokio::main]
@@ -48,4 +83,5 @@ async fn main() {
     let token_address = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; // USDC
     let user_address = "0x54263d2C8D849006C97De20D3Eb9A68D90Fdb5A0"; // mk personal
     interact_with_erc20(&web3, token_address, user_address).await;
+    get_transactions(&web3, user_address).await;
 }
